ab00d10875bd3008814a0c0ef9366bfad9d2bb14
Scott Chamberlain <myrmecocystus@gmail.com>
2011-06-06 08:30:19 -0500

    deleted springer.R and added separate .R files for each of functions for searching metadata, images, and open access material

---
+++ R/springer_images.R
@@ -0,0 +1,36 @@
+# springer_images.R, search Springer images
+
+springer_images <- 
+# Default function to search Springer images
+# Args:
+#   terms: search terms (character)
+#   limit: number of results to return (integer)
+#   startrecord: return results starting at the number specified (integer)
+#   fields: fields to return from search (character) [e.g., 'id,title'], 
+#     any combination of search fields [see plosfields$field] 
+# Examples:
+#   springer_images(terms = 'dna', limit = 5)
+#   springer_images(terms = 'dna', limit = 5, verbose=TRUE) #debug mode
+function(terms, limit, startrecord = NA,
+  url = 'http://api.springer.com/images/json',
+  key = getOption("springerimageskey", stop("need an images API key for Springer Journals")),
+  ...,
+  curl = getCurlHandle()) 
+{  
+  args <- list(api_key = key)
+  if(!is.na(terms))
+    args$q <- terms
+  if(!is.na(limit))
+    args$p <- limit
+  if(!is.na(startrecord))
+    args$s <- startrecord
+  tt <- getForm(url, 
+    .params = args, ...,
+    curl = curl)
+  jsonout <- fromJSON(tt)
+  tempresults <- jsonout$records
+  numres <- length(tempresults) # number of search results
+  names(numres) <- 'Number of search results'
+  dfresults <- data.frame( do.call(rbind, tempresults) )
+  return(list(numres, dfresults))
+}
