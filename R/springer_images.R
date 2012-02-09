ab57b30843ff897e93ce6780c31d7a6b753984a3
Karthik Ram <karthik.ram@gmail.com>
2012-02-09 11:12:25 -0800

    fixed formatting

--- R/springer_images.R
+++ R/springer_images.R
@@ -1,18 +1,18 @@
 #` springer_images.R
-#` 
+#`
 #` Default function to search images in Springer journals
 #` @param "terms" Search terms
 #` @param "limit" Number of results
 #` @param "startrecord" Offset if continuing from earlier search
 #` @param "url" API pattern
 #` @param "key" Your Springer key (available here: http://dev.springer.com/)
-#`   fields: fields to return from search (character) [e.g., 'id,title'], 
-#     any combination of search fields [see plosfields$field] 
+#`   fields: fields to return from search (character) [e.g., 'id,title'],
+#     any combination of search fields [see plosfields$field]
 #` @keywords Literature, Springer
 #` @seealso rplos (https://github.com/ropensci/rplos)
 #` @return List
 #` @alias none
-#` @export 
+#` @export
 #` @examples
 #` springer_images(terms = 'dna', limit = 5)
 #` springer_images(terms = 'dna', limit = 5, verbose=TRUE) #debug mode
@@ -20,8 +20,8 @@ springer_images <-function(terms, limit, startrecord = NA,
   url = 'http://api.springer.com/images/json',
   key = getOption("SpringerImagesKey", stop("need an images API key for Springer Journals")),
   ...,
-  curl = getCurlHandle()) 
-{  
+  curl = getCurlHandle())
+{
   args <- list(api_key = key)
   if(!is.na(terms))
     args$q <- terms
@@ -29,7 +29,7 @@ springer_images <-function(terms, limit, startrecord = NA,
     args$p <- limit
   if(!is.na(startrecord))
     args$s <- startrecord
-  tt <- getForm(url, 
+  tt <- getForm(url,
     .params = args, ...,
     curl = curl)
   jsonout <- fromJSON(tt)
